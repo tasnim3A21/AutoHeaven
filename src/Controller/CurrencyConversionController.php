90ce713f2b5aaee02524c74f4b4e6cf3de9be9e5
raed <mohamedraed.chebbi@esprit.tn>
2025-04-21 01:16:10 +0100

    add dynamic currencies

--- src/Controller/CurrencyConversionController.php
+++ src/Controller/CurrencyConversionController.php
@@ -18,49 +18,47 @@ class CurrencyConversionController extends AbstractController
     }
 
     #[Route('/currency', name: 'currency_conversion')]
-    public function index(Request $request, HttpClientInterface $httpClient): Response
-    {
-        // Get form data
-        $amount = $request->query->get('amount', 0);
-        $selectedCurrency = $request->query->get('currency', 'TND');
-        $convertedAmount = null;
-        $error = null;
+public function index(Request $request, HttpClientInterface $httpClient): Response
+{
+    $amount = $request->query->get('amount', 0);
+    $selectedCurrency = $request->query->get('currency', 'TND');
+    $convertedAmount = null;
+    $error = null;
+    $currencies = [];
 
-        try {
-            if ($amount > 0 && $selectedCurrency !== 'TND') {
-                // Fetch exchange rates
-                $response = $httpClient->request('GET', 'https://api.currencyfreaks.com/v2.0/rates/latest', [
-                    'query' => ['apikey' => $this->apiKey],
-                ]);
-                $data = $response->toArray();
+    try {
+        $response = $httpClient->request('GET', 'https://api.currencyfreaks.com/v2.0/rates/latest', [
+            'query' => ['apikey' => $this->apiKey],
+        ]);
+        $data = $response->toArray();
 
-                if (!isset($data['rates'])) {
-                    throw new \Exception('Invalid response from CurrencyFreaks API.');
-                }
-
-                $rates = $data['rates'];
-                if (!isset($rates[$selectedCurrency]) || !isset($rates['TND'])) {
-                    throw new \Exception("Currency {$selectedCurrency} not supported.");
-                }
-
-                // Convert: TND to USD, then USD to target currency
-                $tndRate = $rates['TND'];
-                $targetRate = $rates[$selectedCurrency];
-                $convertedAmount = ($amount / $tndRate) * $targetRate;
-                $convertedAmount = round($convertedAmount, 2);
-            } elseif ($amount > 0 && $selectedCurrency === 'TND') {
-                $convertedAmount = round($amount, 2);
-            }
-        } catch (\Exception $e) {
-            $error = 'Unable to convert currency: ' . $e->getMessage();
-            $convertedAmount = null;
+        if (!isset($data['rates'])) {
+            throw new \Exception('Invalid response from CurrencyFreaks API.');
         }
 
-        return $this->render('CurrencyConversion/index.html.twig', [
-            'amount' => $amount,
-            'selectedCurrency' => $selectedCurrency,
-            'convertedAmount' => $convertedAmount,
-            'error' => $error,
-        ]);
+        $rates = $data['rates'];
+        $currencies = $rates; // All available currencies
+
+        if ($amount > 0 && $selectedCurrency !== 'TND') {
+            if (!isset($rates[$selectedCurrency]) || !isset($rates['TND'])) {
+                throw new \Exception("Currency {$selectedCurrency} not supported.");
+            }
+
+            $convertedAmount = round(($amount / $rates['TND']) * $rates[$selectedCurrency], 2);
+        } elseif ($amount > 0 && $selectedCurrency === 'TND') {
+            $convertedAmount = round($amount, 2);
+        }
+    } catch (\Exception $e) {
+        $error = 'Unable to convert currency: ' . $e->getMessage();
     }
+
+    return $this->render('CurrencyConversion/index.html.twig', [
+        'amount' => $amount,
+        'selectedCurrency' => $selectedCurrency,
+        'convertedAmount' => $convertedAmount,
+        'error' => $error,
+        'currencies' => $currencies,
+    ]);
+}
+
 }
