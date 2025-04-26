44e6b2158258809df8602020e684d3e6022500a9
raed <mohamedraed.chebbi@esprit.tn>
2025-04-26 14:52:50 +0100

    add PDF export functionality using Dompdf bundle

--- src/Controller/CarDetailsController.php
+++ src/Controller/CarDetailsController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 class CarDetailsController extends AbstractController
 {
@@ -48,5 +50,61 @@ class CarDetailsController extends AbstractController
             'carDetails' => $carDetails,
             'error' => $error,
         ]);
+    }
+
+    #[Route('/car/details/export-pdf', name: 'car_details_export_pdf')]
+    public function exportPdf(Request $request, HttpClientInterface $httpClient): Response
+    {
+        $make = $request->query->get('make', '');
+        $model = $request->query->get('model', '');
+        $carDetails = [];
+        $error = null;
+
+        if ($make && $model) {
+            try {
+                $response = $httpClient->request('GET', 'https://api.api-ninjas.com/v1/cars', [
+                    'headers' => [
+                        'X-Api-Key' => $this->apiKey,
+                    ],
+                    'query' => [
+                        'make' => $make,
+                        'model' => $model,
+                    ],
+                ]);
+                $carDetails = $response->toArray();
+            } catch (\Exception $e) {
+                $error = 'Unable to fetch car details: ' . $e->getMessage();
+            }
+        }
+
+        // Configure Dompdf
+        $options = new Options();
+        $options->set('defaultFont', 'Arial');
+        $options->set('isRemoteEnabled', true);
+        $options->set('isHtml5ParserEnabled', true);
+        
+        $dompdf = new Dompdf($options);
+
+        // Render the PDF template
+        $html = $this->renderView('car_details/export_pdf.html.twig', [
+            'make' => $make,
+            'model' => $model,
+            'carDetails' => $carDetails,
+            'error' => $error,
+        ]);
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        // Stream the PDF
+        return new Response(
+            $dompdf->output(),
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'attachment; filename="recherche-voiture-'.$make.'-'.$model.'.pdf"',
+            ]
+        );
     }
 }
