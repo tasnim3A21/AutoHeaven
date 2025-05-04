f439467145d19c6789f82bc61ee92a0020081d09
Rayen <rayen.elfil@esprit.tn>
2025-05-04 09:34:54 +0100

    metier calendrier

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -9,7 +9,8 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Voiture;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Reservation;
+use App\Entity\Res_testdrive;
+use App\Entity\User;
 
 
 final class CarController extends AbstractController
@@ -23,38 +24,41 @@ final class CarController extends AbstractController
             'voitures' => $voitures,
         ]);
     }
+
     #[Route('/car/{id}/details', name: 'car_details', methods: ['GET'])]
     public function details(Voiture $voiture): Response
-{
-    if (!$voiture) {
-        return new Response('Car not found', 404);
-    }
-
-    dump($voiture);  // Ensure the voiture data is being passed correctly
-    return $this->render('car/details.html.twig', [
-        'voiture' => $voiture,
-    ]);
-}
-    #[Route('/car/{id}/reserver', name: 'car_reservation', methods: ['GET', 'POST'])]
-    public function reserver(Voiture $voiture, Request $request, EntityManagerInterface $entityManager)
     {
         if (!$voiture) {
             return new Response('Car not found', 404);
         }
+        dump($voiture);  // Ensure the voiture data is being passed correctly
+        return $this->render('car/details.html.twig', [
+            'voiture' => $voiture,
+        ]);
+    }
+    
+    #[Route('/car/{id}/reserver/{user}', name: 'car_reservation', methods: ['GET', 'POST'])]
+    public function reserver(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $idV = $request->request->get('id_v');
+        $clientId = $request->request->get('client_id');
+        $reservationDate = $request->request->get('reservation_date');
 
-        if ($request->isMethod('POST')) {
-            $reservation = new Reservation();
-            $voiture = $entityManager->getRepository(Voiture::class)->find($request->request->get('id_v'));
-            $reservation->setId_v($voiture->getId_v());
-            $connectedUser = $this->getUser();
-            $reservation->setId($connectedUser->getUserIdentifier());
-            $reservation->setDate_res(new \DateTime($request->request->get('reservation_date')));
+        $voiture = $entityManager->getRepository(Voiture::class)->find($idV);
+        $user = $entityManager->getRepository(User::class)->find($clientId);
 
-            $entityManager->persist($reservation);
-            $entityManager->flush();
-
-            // Return a JSON response indicating success
-            return $this->json(['status' => 'success', 'message' => 'Reservation successful']);
+        if (!$voiture || !$user) {
+            return new Response('Car or User not found', 404);
         }
+
+        $reservation = new Res_testdrive();
+        $reservation->setIdV($voiture->getIdV());
+        $reservation->setIdU($user->getId());
+        $reservation->setDate(new \DateTime($reservationDate));
+
+        $entityManager->persist($reservation);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('car_details', ['id' => $voiture->getIdV()]);
     }
 }
